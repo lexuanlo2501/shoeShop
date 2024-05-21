b59475e26c4abd66f19552b88616f493f5477660
lxt <lexuanlo2501@gmail.com>
2024-05-21 21:44:59 +0700

    update

--- src/page/InforUser/index.js
+++ src/page/InforUser/index.js
@@ -64,6 +64,7 @@ function InforUser() {
     }
     // pass_confirm,newPass
     const schema = yup.object().shape({
+        phoneNumber: yup.string().matches(/^[0-9]+$/, "Số điện thoại chỉ được chứa các ký tự số").length(10, "Số điện thoại phải có 10 số"),
         newPass: yup.string(),
         pass_confirm: yup.string().oneOf([yup.ref("newPass")], "Mật khẩu không khớp"),
     }).required();
@@ -245,6 +246,8 @@ function InforUser() {
                                 setStatusChange(pre => ({...pre, phoneNumber:!pre.phoneNumber}))
                             }}
                         >{statusChange.phoneNumber ?  "Đóng" : "thay đổi"}</button>
+                    {errors.phoneNumber && <p style={{"color": "#ee5353",'textAlign': 'center'}}>{errors.phoneNumber?.message}</p>}
+
                     </div>
                     <div className={cx('input_update')}>
                         <label>Mật khẩu :</label> <span className={cx('valueClkToChange')}>************</span>
